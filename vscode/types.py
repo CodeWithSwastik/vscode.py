6c37c71457edb631465bbe98a93b122dd3e73a78
unknown <codewithswastik@gmail.com>
2021-07-31 16:18:51 +0530

    Add Types to docs

--- vscode/types.py
+++ vscode/types.py
@@ -1,6 +1,7 @@
 import json
 
 from .utils import *
+from typing import Optional
 
 
 class ActivityBar:
@@ -9,6 +10,12 @@ class ActivityBar:
     """
 
     def __init__(self, id: str, title: str, icon: str) -> None:
+        """
+        Args:
+            id: The unique id of the activity bar.
+            title: The title shown to the user.
+            icon: The icon of the activity bar.
+        """
         self.id = id
         self.title = title
         self.icon = icon
@@ -19,7 +26,13 @@ class StaticWebview:
     Content settings for a Static Webview.
     """
 
-    def __init__(self, id: str, html: str, title: str = None) -> None:
+    def __init__(self, id: str, html: str, title: Optional[str] = None) -> None:
+        """
+        Args:
+            id: The unique id of the static webview.
+            title: The title shown to the user.
+            html: The html to rendered in the webview.
+        """
         self.id = id
         self.html = html
         self.title = title
