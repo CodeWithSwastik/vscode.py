f78c3384a10d49c4a9f66d3c22a7f9997abfed45
unknown <codewithswastik@gmail.com>
2021-12-21 15:13:10 +0530

    Boilerplate for compiler

--- vscode/compiler.py
+++ vscode/compiler.py
@@ -1,4 +1,20 @@
-from .extension import Extension
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from .extension import Extension
+
+def create_package_json(data: dict, config: dict) -> dict:
+    package_name = data["name"]
+    package = {
+        "name": package_name,
+        "displayName": data.get("display_name", package_name),
+        "version": data["version"],
+        "engines": "^1.58.0",
+        "categories": "Other",
+        "main": "./build/extension.js",
+    }
+    package.update(config)
+    return package
 
 def build(extension: Extension) -> None:
     pass
