83d1773979003cfee8c60fbdee2689bb96bb0dad
unknown <codewithswastik@gmail.com>
2021-12-21 14:02:20 +0530

    Remove old files, bump version

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-version = "1.5.4"
+version = "2.0.0"
  
 with open("README.md", "r", encoding="utf-8") as readme_file:
     long_description = readme_file.read()
--- vscode/__init__.py
+++ vscode/__init__.py
@@ -2,13 +2,4 @@ __author__ = "Swas.py"
 __title__ = "vscode"
 __license__ = "MIT"
 __copyright__ = "Copyright 2021 Swas.py"
-__version__ = "1.5.4"
-
-from . import window
-from .compiler import *
-from .extension import *
-from .themes import *
-from .env_methods import *
-from . import types as ext
-from .types import *
-from .webviews import *
+__version__ = "2.0.0"
