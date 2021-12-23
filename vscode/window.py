5c44cadd2305893cda06749b45971c39b6a0b38f
unknown <codewithswastik@gmail.com>
2021-12-23 21:38:49 +0530

    Add QuickPickOptions

--- vscode/window.py
+++ vscode/window.py
@@ -17,6 +17,7 @@ __all__ = (
     "Terminal",
     "QuickPick",
     "QuickPickItem",
+    "QuickPickOptions",
     "InputBox",
     "WindowState",
     "Message",
@@ -206,31 +207,85 @@ class QuickInput:
         pass
 
 
-@dataclass
 class QuickPickItem:
-    label: str
-    always_show: Optional[bool] = None
-    description: Optional[str] = None
-    detail: Optional[str] = None
-    picked: Optional[bool] = None
+    def __init__(
+        self,
+        label: str,
+        always_show: Optional[bool] = None,
+        description: Optional[str] = None,
+        detail: Optional[str] = None,
+        picked: Optional[bool] = None,
+        **kwargs
+    ):
+        self.label = label
+        self.always_show = always_show or kwargs.pop("alwaysShow", None)
+        self.detail = detail
+        self.description = description
+        self.picked = picked
+        self.__dict__.update(kwargs)
+
+    def to_dict(self) -> dict:
+        return {snake_case_to_camel_case(k): v for k, v in self.__dict__.items()}
+
+
+class QuickPickOptions:
+    """
+    Options to configure the behavior of the quick pick UI.
+    """
+
+    def __init__(
+        self,
+        title: Optional[str] = None,
+        can_pick_many: Optional[bool] = None,
+        ignore_focus_out: Optional[bool] = None,
+        match_on_description: Optional[bool] = None,
+        place_holder: Optional[str] = None,
+        match_on_detail: Optional[bool] = None,
+    ) -> None:
+        """
+        Args:
+            title: An optional string that represents the title of the quick pick.
+            can_pick_many: An optional flag to make the picker accept multiple selections, if true the result is an array of picks.
+            ignore_focus_out: Set to True to keep the input box open when focus moves to another part of the editor or to another window. This setting is ignored on iPad and is always False.
+            place_holder: An optional string to show as placeholder in the input box to guide the user what to type.
+            match_on_description: An optional flag to include the description when filtering the picks.
+            match_on_detail: An optional flag to include the detail when filtering the picks.
+        """
+        self.title = title
+        self.can_pick_many = can_pick_many
+        self.ignore_focus_out = ignore_focus_out
+        self.match_on_description = match_on_description
+        self.place_holder = place_holder
+        self.match_on_detail = match_on_detail
+
+    def to_dict(self) -> dict:
+        return {snake_case_to_camel_case(k): v for k, v in self.__dict__.items()}
 
 
 class QuickPick(Showable, QuickInput):
     def __init__(
-        self, items: List[str, QuickPickItem]
-    ) -> None:  # TODO: add options as kwargs
+        self, 
+        items: List[str, QuickPickItem], 
+        options: Optional[QuickPickOptions] = None
+    ) -> None:  
         self.items = [QuickPickItem(i) if isinstance(i, str) else i for i in items]
+        self.options = options
 
-    async def _show(self, ws) -> Optional[QuickPickItem]:
-        options = [
-            {snake_case_to_camel_case(k): v for k, v in i.__dict__.items()} for i in self.items
+    async def _show(self, ws) -> Optional[Union[QuickPickItem, List[QuickPickItem]]]:
+        items = [
+            i.to_dict() for i in self.items
         ]
-        print(json.dumps(options))
+
+        options = ','+json.dumps(self.options.to_dict()) if self.options else ''
+
         chosen = await ws.run_code(
-            f"vscode.window.showQuickPick({json.dumps(options)})",
+            f"vscode.window.showQuickPick({json.dumps(items)}{options})",
         )
         if chosen:
-            return QuickPickItem(**{camel_case_to_snake_case(k): v for k, v in chosen.items()})
+            if isinstance(chosen, dict):
+                return QuickPickItem(**chosen)
+            else:
+                return [QuickPickItem(**r) for r in chosen]
 
 
 class InputBox(Showable, QuickInput):
