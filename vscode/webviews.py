0896b09d180a96d79a2e38e828551c211e878499
Swas.py <codewithswastik@gmail.com>
2023-11-29 23:22:55 +0530

    Add preserve_focus in reveal() method

--- vscode/webviews.py
+++ vscode/webviews.py
@@ -57,19 +57,25 @@ class WebviewPanel:
         message = json.dumps(data)
         await self.ws.run_code(f"webviews['{self.id}'].webview.postMessage({message})", wait_for_response=False)
 
-    async def reveal(self, column: Optional[ViewColumn] = None) -> None:
+    async def reveal(self, column: Optional[ViewColumn] = None, preserve_focus: bool = False) -> None:
         column = column or self.column
 
         if not self.running:
             raise ValueError(f"Webview is not running")
         
-        await self.ws.run_code(f"webviews['{self.id}'].webview.reveal({column})", wait_for_response=False)
+        await self.ws.run_code(f"webviews['{self.id}'].webview.reveal({column}, {str(preserve_focus).lower()})", wait_for_response=False)
 
     async def is_visible(self) -> bool:
         if not self.running:
             raise ValueError(f"Webview is not running")
         
         return await self.ws.run_code(f"webviews['{self.id}'].webview.visible", thenable=False)
+    
+    async def is_active(self) -> bool:
+        if not self.running:
+            raise ValueError(f"Webview is not running")
+        
+        return await self.ws.run_code(f"webviews['{self.id}'].webview.active", thenable=False)
     
     async def handle_event(self, name: str, data: Optional[dict] = None) -> None:
         if name == "message":
