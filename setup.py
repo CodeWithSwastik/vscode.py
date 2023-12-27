4f57c1a41394f867426ee0094e4f8d699453d5f8
Swas.py <codewithswastik@gmail.com>
2023-12-27 09:03:56 +0530

    Version bump

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import os
 from setuptools import setup, find_packages
 
-version = "2.0.0a5"
+version = "2.0.0.beta1"
 
 with open("README.md", "r", encoding="utf-8") as readme_file:
     long_description = readme_file.read()
@@ -28,7 +28,7 @@ setup(
     },
     classifiers=[
         "Programming Language :: Python :: 3",
-        "Development Status :: 3 - Alpha",
+        "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
--- vscode/__init__.py
+++ vscode/__init__.py
@@ -2,7 +2,7 @@ __author__ = "Swas.py"
 __title__ = "vscode"
 __license__ = "MIT"
 __copyright__ = "Copyright 2021-2023 Swas.py"
-__version__ = "2.0.0a5"
+__version__ = "2.0.0.beta1"
 
 from .extension import *
 from .compiler import *
